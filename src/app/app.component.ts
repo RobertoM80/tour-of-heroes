255a93ed7cf7179352e1ac9b027b0b0a1aa702a0
Roberto Mirabella <bronzetto@outlook.com>
2017-09-23 20:25:35 +0100

    tried routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  title = 'Tour of heroes routing!!!!';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService){}
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
